44793f61f0b0bb513e4ec0c1445f1a61084aaa0b
chamo <1107191419@qq.com>
2019-04-26 16:33:05 +0800

    新增了1道题的解法。

---
+++ EOJ/2968. 付钱.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void cal(int temp) {
+	int i = 0;
+	int a[] = { 100,50,20,10,5,1 };
+	while (i <= 4) {
+		cout << temp / a[i] << " ";
+		temp %= a[i];
+		i++;
+	}
+	cout << temp << endl;
+}
+
+int main() {
+	int question_num;
+	int temp;
+	cin >> question_num;
+	for (int i = 0;i < question_num;i++) {
+		cout << "case #" << i << ":" << endl;
+		cin >> temp;
+		cal(temp);
+	}
+	return 0;
+}
