4bdfc05402c45869aeef66e3d81b19ba0c26503d
chamo <1107191419@qq.com>
2019-04-27 13:41:45 +0800

    添加了问题3686的解答。 添加了新方法，能够先输入问题序号再调用相应的函数。

--- EOJ/main.cpp
+++ EOJ/main.cpp
@@ -2,7 +2,21 @@
 #include "solutions.h"
 using namespace std;
 
+int solution(int index) {
+	switch (index)
+	{
+	case 1026:solution_1026;break;
+	case 1073:solution_1073;break;
+	case 2968:solution_2968;break;
+	case 3686:solution_3686;break;
+
+	default:break;
+	}
+}
+
 int main() {		//美中不足的是现在只可以从标准输入中获取数值，因为如果采用fopen等函数会大幅修改各个solution。
-	solution_1073();
+	int question_num;
+	cin >> question_num;
+	solution(question_num);
 	return 0;
 }
--- EOJ/solutions.h
+++ EOJ/solutions.h
@@ -3,3 +3,4 @@
 int solution_1073();
 int solution_2968();
 int solution_1026();
+int solution_3686();
